7ed26940c230244190fb134fd44bbf96d57dac80
Serdar Doğan <eraydogan259@gmail.com>
2022-06-09 14:18:06 +0300

    Catch The Kenny
    Java'da öğrendiklerimi uygulamalar üzerinde göstermeye çalışıyorum.

---
+++ CatchTheKenny/app/src/main/java/com/example/catchthekenny/kennyonclick.java
@@ -0,0 +1,38 @@
+package com.example.catchthekenny;
+
+import android.view.View;
+
+
+public class kennyonclick  {
+
+    public void kenny1(View view){
+
+    }
+
+    public void kenny2(View view){
+
+    }
+
+    public void kenny3(View view){
+
+    }
+
+    public void kenny4(View view){
+
+    }
+
+    public void kenny5(View view){
+
+    }
+
+    public void kenny6(View view){
+
+    }
+
+    public void kenny7(View view){
+
+    }
+    public void kenny8(View view){
+
+    }
+}
